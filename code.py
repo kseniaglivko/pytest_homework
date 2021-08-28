2a518f7d6b3387a1338298f5a9ab7258942c8222
kseniaglivko <k.glivko@mail.ru>
2021-08-28 20:24:39 +0300

    Initial commit.

---
+++ code.py
@@ -0,0 +1,22 @@
+"""Модуль с тестируемым кодом."""
+import re
+
+def valid_email(email: str) -> bool:
+    """
+    Check is email correct.
+
+    :param email:
+    :return: bool
+    """
+    return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+
+
+def log(file_name: str, text: str) -> None:
+    """
+    Write log to file.
+
+    :param file_name:
+    :param text:
+    """
+    with open(file_name, "w") as f_obj:
+        f_obj.write(text)
